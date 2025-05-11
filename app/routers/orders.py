92c13dd2c6b715969e2e10eca42de7c4296dd288
Shahmir675 <shahmirkhan9181@gmail.com>
2025-05-11 16:39:17 +0500

    Removed unncessary imports

--- app/routers/orders.py
+++ app/routers/orders.py
@@ -8,7 +8,7 @@ from app.core.dependencies import get_db
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
-@router.get("/", response_model=List[pyd_models.OrderResponse])
+@router.get("", response_model=List[pyd_models.OrderResponse])
 async def get_orders(session: AsyncSession = Depends(get_db)):
     result = await session.execute(select(sql_models.Order))
     orders = result.scalars().all()
