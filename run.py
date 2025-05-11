8fe54eeada66fab096f8462848b8434a3bb5018b
Shahmir675 <shahmirkhan9181@gmail.com>
2025-05-11 16:37:38 +0500

    Removed the reload parameter on code change

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import uvicorn
 
 if __name__ == "__main__":
     print("Starting OptiTrade server...")
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8001)
